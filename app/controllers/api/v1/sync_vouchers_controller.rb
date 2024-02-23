738cc9e8899d08c5d80295d0e892bba3e21fe345
Thai Dang <thai.hulk@gmail.com>
2024-02-23 21:42:02 +0700

    Update sync_vouchers_controller.rb

--- app/controllers/api/v1/sync_vouchers_controller.rb
+++ app/controllers/api/v1/sync_vouchers_controller.rb
@@ -6,17 +6,13 @@ module API
       before_action :require_customer!
 
       def create
-        shift = @customer.shifts.find_by(no: shift_no)
+        shift = @customer.shifts.find_by(no: params[:shift_no])
 
         ActiveRecord::Base.transaction do
           vouchers.each do |voucher|
             shift.vouchers.create!(voucher)
           end
         end
-      end
-
-      def shift_no
-        params.permit(:shift_no)
       end
 
       def vouchers
